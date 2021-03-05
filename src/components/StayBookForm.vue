283b66bee76d336c1d76f64d4a2e0cc30131fb3b
a.sobieraj <arsobieraj@gmail.com>
2021-03-05 22:41:49 +0100

    add star component

--- src/components/StayBookForm.vue
+++ src/components/StayBookForm.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>Stay book form
-    <StarRating />
-    <DatePicker />
-  </div>
+  <form :onsubmit="handleSubmit">
+
+    <StarRating
+      :star-value="starValue"
+      :votes-number="votesNumber"
+    />
+    <DatePicker v-if="false" />
+
+    <button type="submit">Submit</button>
+  </form>
+
 </template>
 
 <script>
@@ -10,8 +17,30 @@ import DatePicker from "./base/DatePicker.vue";
 import StarRating from "./base/StarRating.vue";
 export default {
   components: { StarRating, DatePicker },
+  data() {
+    return {
+      starValue: 3.3,
+      votesNumber: 123,
+    };
+  },
+  methods: {
+    testFun() {
+      console.log("TESTSETEST");
+    },
+    handleSubmit() {
+      console.log("HANDLE SUBMIT");
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+form {
+  min-height: 200px;
+  width: 50%;
+  margin: auto 50px;
+  border: 2px solid var(--border);
+  display: flex;
+  flex-direction: column;
+}
 </style>
--- src/components/base/StarRating.vue
+++ src/components/base/StarRating.vue
@@ -1,10 +1,54 @@
 <template>
-  <div>Star rating</div>
+  <div
+    class="Stars"
+    :style="{'--rating': starValue}"
+    :aria-label="`Rating of this product is ${starValue} out of 5.`"
+  >
+    <span
+      class="votes-label"
+      :aria-label="`${votesNumber} votes`"
+    >{{votesNumber}}</span>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    starValue: {
+      type: Number,
+      require: true,
+    },
+    votesNumber: {
+      type: Number,
+      require: true,
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+.Stars {
+  --percent: calc(var(--rating) / 5 * 100%);
+
+  display: inline;
+  font-size: var(--star-size);
+
+  &::before {
+    content: "★★★★★";
+    letter-spacing: -1px;
+    background: linear-gradient(
+      90deg,
+      var(--star-color) var(--percent),
+      var(--star-background) var(--percent)
+    );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+}
+
+.votes-label {
+  padding-left: 5px;
+  font-size: 0.825rem;
+  font-weight: bold;
+}
 </style>
